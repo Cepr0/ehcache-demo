12068b6259f85986cf4b640c3f2a45dc5ee245ce
Cepr0 <cepr0@ukr.net>
2018-03-21 01:10:48 +0200

    Added beans for ehcache

--- src/main/java/io/github/cepr0/demo/Application.java
+++ src/main/java/io/github/cepr0/demo/Application.java
@@ -4,8 +4,13 @@ import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ClassPathResource;
 
 import java.sql.SQLException;
 
@@ -15,6 +20,7 @@ import static io.github.cepr0.demo.Role.ADMIN;
 import static io.github.cepr0.demo.Role.USER;
 import static java.util.Arrays.asList;
 
+@EnableCaching
 @SpringBootApplication
 public class Application {
 
@@ -31,6 +37,19 @@ public class Application {
 	@Bean(initMethod = "start", destroyMethod = "stop")
 	public Server h2Server() throws SQLException {
 		return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+	}
+
+	@Bean
+	public CacheManager cacheManager() {
+		return new EhCacheCacheManager(ehCacheCacheManager().getObject());
+	}
+
+	@Bean
+	public EhCacheManagerFactoryBean ehCacheCacheManager() {
+		EhCacheManagerFactoryBean factoryBean = new EhCacheManagerFactoryBean();
+		factoryBean.setConfigLocation(new ClassPathResource("ehcache.xml"));
+		factoryBean.setShared(true);
+		return factoryBean;
 	}
 
 	@EventListener
--- src/main/java/io/github/cepr0/demo/ParentRepo.java
+++ src/main/java/io/github/cepr0/demo/ParentRepo.java
@@ -1,7 +1,9 @@
 package io.github.cepr0.demo;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,4 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RepositoryRestResource//(exported = false)
 @Transactional(readOnly = true)
 public interface ParentRepo extends JpaRepository<Parent, Integer>, JpaSpecificationExecutor<Parent> {
+
+	@Cacheable("io.github.cepr0.demo.Parent")
+	Parent getById(@Param("id") Integer id);
 }
